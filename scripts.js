8323157d7312a7866909df663656b7ab677943e8
Eduardo Tavares <eduardot.developer@gmail.com>
2025-02-09 17:04:42 -0300

    comentários em javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+// O console.log exibe no console um texto.
+console.log("Hello World") // Também posso colocar do lado.
+
+/*
+  Comentário de múltiplas linhas.
+
+  Comentários são trecho de texto que podem ser adicionados junto com o código.
+
+  O navegador ignora o texto marcado como comentário.
+
+  Porque utilizar comentários durante seus estudos:
+  - Fornecer informações adicionais sobre o que o código faz;
+  - Deixar lembretes de coisas a fazer;
+  - Relembrar no futuro o que aquilo faz;
+  - Etc.
+*/
